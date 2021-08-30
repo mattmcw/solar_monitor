c3c299bea353888701fbc4663575bef96128f42b
mattmcw <matt@sixteenmillimeter.com>
2021-08-30 15:07:18 -0400

    Print on separate lines

--- src/fona/battery.sh
+++ src/fona/battery.sh
@@ -6,4 +6,5 @@ CHARGE=`echo "${RESPONSE}"  | awk -F',' '{print $2}'`
 VOLTAGE=`echo "${RESPONSE}" | awk -F',' '{print $3}'`
 #VOLTAGE=`echo "scale=3;${VOLTAGE}/1000;" | bc`
 
-echo "${CHARGE}% ${VOLTAGE}V"
+echo "charge=${CHARGE}"
+echo "voltage=${VOLTAGE}"
