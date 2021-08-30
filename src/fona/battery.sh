007f13b3e5a9ceec3e47f2a9b3acb5c259407954
mattmcw <matt@sixteenmillimeter.com>
2021-08-30 15:08:06 -0400

    Script without division, fine for now

--- src/fona/battery.sh
+++ src/fona/battery.sh
@@ -4,7 +4,7 @@ RESPONSE=`bash ./src/fona/send.sh "AT+CBC" | grep "CBC:"`
 
 CHARGE=`echo "${RESPONSE}"  | awk -F',' '{print $2}'`
 VOLTAGE=`echo "${RESPONSE}" | awk -F',' '{print $3}'`
-#VOLTAGE=`echo "scale=3;${VOLTAGE}/1000;" | bc`
+#VOLTAGE=`echo "scale=3;${VOLTAGE}/1000" | bc`
 
-echo "charge=${CHARGE}"
-echo "voltage=${VOLTAGE}"
+echo "charge[%]=${CHARGE}"
+echo "voltage[mV]=${VOLTAGE}"
