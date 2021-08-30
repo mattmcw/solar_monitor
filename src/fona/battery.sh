295f11ca028007e94b070e8ebb18f7ce4891f855
mattmcw <matt@sixteenmillimeter.com>
2021-08-30 15:06:18 -0400

    Try simple parsing line

--- src/fona/battery.sh
+++ src/fona/battery.sh
@@ -3,7 +3,7 @@
 RESPONSE=`bash ./src/fona/send.sh "AT+CBC" | grep "CBC:"`
 
 CHARGE=`echo "${RESPONSE}"  | awk -F',' '{print $2}'`
-#VOLTAGE=`echo "${RESPONSE}" | awk -F',' '{print $3}'`
+VOLTAGE=`echo "${RESPONSE}" | awk -F',' '{print $3}'`
 #VOLTAGE=`echo "scale=3;${VOLTAGE}/1000;" | bc`
 
 echo "${CHARGE}% ${VOLTAGE}V"
