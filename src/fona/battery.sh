bd52dc27453367f79d8a1b2c871ea82c387aa867
mattmcw <matt@sixteenmillimeter.com>
2021-08-30 15:03:57 -0400

    Parsing error

--- src/fona/battery.sh
+++ src/fona/battery.sh
@@ -6,4 +6,4 @@ CHARGE=`echo "${RESPONSE}"  | awk -F',' '{print $2}'`
 VOLTAGE=`echo "${RESPONSE}" | awk -F',' '{print $3}'`
 VOLTAGE=`echo "scale=3;${VOLTAGE}/1000;" | bc`
 
-echo "${CHARGE}\% [${VOLTAGE} V]"
+echo "${CHARGE}% ${VOLTAGE}V"
