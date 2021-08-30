73ba2a0940de0f54bc6ee63afdc8564e69f79b93
mattmcw <matt@sixteenmillimeter.com>
2021-08-30 14:58:59 -0400

    Parse response from FONA

--- src/fona/battery.sh
+++ src/fona/battery.sh
@@ -2,4 +2,7 @@
 
 RESPONSE=`bash ./src/fona/send.sh "AT+CBC"`
 
-echo "${RESPONSE}" | grep "CBC:"
+CHARGE=`echo "${RESPONSE}" | grep "CBC:" | awk -F',' '{print $2}'`
+VOLTAGE=`echo "${RESPONSE}" | grep "CBC:" | awk -F',' '{print $3}'`
+
+echo "${CHARGE}% [${VOLTAGE} mV]"
